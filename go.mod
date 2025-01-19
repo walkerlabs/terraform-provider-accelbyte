0587650d6d565fac81cc3f229d81758c9146d22a
Mikael Kalms <mikael@kalms.org>
2025-01-19 09:54:53 +0100

    Login to AccelByte backend is working

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/walkerlabs/terraform-provider-accelbyte
 go 1.22.7
 
 require (
+	github.com/AccelByte/accelbyte-go-sdk v0.76.0
 	github.com/hashicorp/terraform-plugin-framework v1.13.0
 	github.com/hashicorp/terraform-plugin-go v0.25.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0
@@ -10,13 +11,31 @@ require (
 )
 
 require (
+	github.com/AccelByte/bloom v0.0.0-20180915202807-98c052463922 // indirect
+	github.com/AccelByte/go-jose v2.1.4+incompatible // indirect
 	github.com/ProtonMail/go-crypto v1.1.0-alpha.2 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/agext/levenshtein v1.2.2 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/fatih/color v1.16.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-openapi/analysis v0.21.2 // indirect
+	github.com/go-openapi/errors v0.20.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/loads v0.21.1 // indirect
+	github.com/go-openapi/runtime v0.19.29 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/strfmt v0.21.1 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/go-openapi/validate v0.21.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -36,7 +55,8 @@ require (
 	github.com/hashicorp/terraform-registry-address v0.2.3 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
@@ -45,14 +65,25 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/oklog/run v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/willf/bitset v1.1.11 // indirect
 	github.com/zclconf/go-cty v1.15.0 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
+	go.opentelemetry.io/contrib/propagators/aws v1.15.0 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.14.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/mod v0.21.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
@@ -61,4 +92,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -5,14 +5,19 @@ package provider
 
 import (
 	"context"
-	"net/http"
+	"os"
 
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/factory"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service/iam"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
 	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // Ensure AccelByteProvider satisfies various provider interfaces.
@@ -30,9 +35,11 @@ type AccelByteProvider struct {
 
 // AccelByteProviderModel describes the provider data model.
 type AccelByteProviderModel struct {
-	/*
-		Endpoint types.String `tfsdk:"endpoint"`
-	*/
+	BaseUrl         types.String `tfsdk:"base_url"`
+	IamClientId     types.String `tfsdk:"iam_client_id"`
+	IamClientSecret types.String `tfsdk:"iam_client_secret"`
+	AdminUsername   types.String `tfsdk:"admin_username"`
+	AdminPassword   types.String `tfsdk:"admin_password"`
 }
 
 func (p *AccelByteProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -42,14 +49,30 @@ func (p *AccelByteProvider) Metadata(ctx context.Context, req provider.MetadataR
 
 func (p *AccelByteProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		/*
-			Attributes: map[string]schema.Attribute{
-				"endpoint": schema.StringAttribute{
-					MarkdownDescription: "Example provider attribute",
-					Optional:            true,
-				},
+		Attributes: map[string]schema.Attribute{
+			"base_url": schema.StringAttribute{
+				MarkdownDescription: "URL to AccelByte cluster, typically on the form 'https://<something>.accelbyte.io'",
+				Required:            true,
 			},
-		*/
+			"iam_client_id": schema.StringAttribute{
+				MarkdownDescription: "IAM Client ID to use for authentication",
+				Required:            true,
+			},
+			"iam_client_secret": schema.StringAttribute{
+				MarkdownDescription: "IAM Client Secret to use for authentication",
+				Required:            true,
+				Sensitive:           true,
+			},
+			"admin_username": schema.StringAttribute{
+				MarkdownDescription: "Admin user email to use for authentication",
+				Required:            true,
+			},
+			"admin_password": schema.StringAttribute{
+				MarkdownDescription: "Admin user password to use for authentication",
+				Required:            true,
+				Sensitive:           true,
+			},
+		},
 	}
 }
 
@@ -62,13 +85,212 @@ func (p *AccelByteProvider) Configure(ctx context.Context, req provider.Configur
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	// If practitioner provided a configuration value for any of the
+	// attributes, it must be a known value.
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	if data.BaseUrl.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("base_url"),
+			"Unknown AccelByte API base_url",
+			"The provider cannot create the AccelByte API client as the base_url nas not been given.. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ACCELBYTE_BASE_URL environment variable.",
+		)
+	}
+
+	if data.IamClientId.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("iam_client_id"),
+			"Unknown AccelByte API iam_client_id",
+			"The provider cannot create the AccelByte API client as the iam_client_id nas not been given.. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ACCELBYTE_IAM_CLIENT_ID environment variable.",
+		)
+	}
+
+	if data.IamClientSecret.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("iam_client_secret"),
+			"Unknown AccelByte API iam_client_secret",
+			"The provider cannot create the AccelByte API client as the iam_client_secret nas not been given.. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ACCELBYTE_IAM_CLIENT_SECRET environment variable.",
+		)
+	}
+
+	if data.AdminUsername.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("admin_username"),
+			"Unknown AccelByte API admin_username",
+			"The provider cannot create the AccelByte API client as the admin_username nas not been given.. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ACCELBYTE_ADMIN_USERNAME environment variable.",
+		)
+	}
+
+	if data.AdminPassword.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("admin_password"),
+			"Unknown AccelByte API admin_password",
+			"The provider cannot create the AccelByte API client as the admin_password nas not been given.. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the ACCELBYTE_ADMIN_PASSWORD environment variable.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Default values to environment variables, but override
+	// with Terraform configuration value if set.
+
+	baseUrl := os.Getenv("ACCELBYTE_BASE_URL")
+	iamClientId := os.Getenv("ACCELBYTE_IAM_CLIENT_ID")
+	iamClientSecret := os.Getenv("ACCELBYTE_IAM_CLIENT_SECRET")
+	adminUsername := os.Getenv("ACCELBYTE_ADMIN_USERNAME")
+	adminPassword := os.Getenv("ACCELBYTE_ADMIN_PASSWORD")
+
+	if !data.BaseUrl.IsNull() {
+		baseUrl = data.BaseUrl.ValueString()
+	}
+
+	if !data.IamClientId.IsNull() {
+		iamClientId = data.IamClientId.ValueString()
+	}
+
+	if !data.IamClientSecret.IsNull() {
+		iamClientSecret = data.IamClientSecret.ValueString()
+	}
+
+	if !data.AdminUsername.IsNull() {
+		adminUsername = data.AdminUsername.ValueString()
+	}
+
+	if !data.AdminPassword.IsNull() {
+		adminPassword = data.AdminPassword.ValueString()
+	}
+
+	// If any of the expected configurations are missing, return
+	// errors with provider-specific guidance.
+
+	if baseUrl == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("base_url"),
+			"Missing HashiCups API base_url",
+			"The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API base_url. "+
+				"Set the base_url value in the configuration or use the ACCELBYTE_BASE_URL environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if iamClientId == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("iam_client_id"),
+			"Missing HashiCups API iam_client_id",
+			"The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API iam_client_id. "+
+				"Set the iam_client_id value in the configuration or use the ACCELBYTE_IAM_CLIENT_ID environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if iamClientSecret == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("iam_client_secret"),
+			"Missing HashiCups API iam_client_secret",
+			"The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API iam_client_secret. "+
+				"Set the iam_client_secret value in the configuration or use the ACCELBYTE_IAM_CLIENT_SECRET environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if adminUsername == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("admin_username"),
+			"Missing HashiCups API admin_username",
+			"The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API admin_username. "+
+				"Set the admin_username value in the configuration or use the ACCELBYTE_ADMIN_USERNAME environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if adminPassword == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("admin_password"),
+			"Missing HashiCups API admin_password",
+			"The provider cannot create the HashiCups API client as there is a missing or empty value for the HashiCups API admin_password. "+
+				"Set the admin_password value in the configuration or use the ACCELBYTE_ADMIN_PASSWORD environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Configure Base URL (i.e. which cluster), and IAM Client/Secret pair
+	// These will later on be used during the LoginUser call
+
+	configRepository := auth.ConfigRepositoryImpl{
+		ClientId:     iamClientId,
+		ClientSecret: iamClientSecret,
+		BaseUrl:      baseUrl,
+	}
+
+	tokenRepository := auth.DefaultTokenRepositoryImpl()
+
+	oAuth20Service := &iam.OAuth20Service{
+		Client:           factory.NewIamClient(&configRepository),
+		ConfigRepository: &configRepository,
+		TokenRepository:  tokenRepository,
+		RefreshTokenRepository: &auth.RefreshTokenImpl{ // Automatically refresh the token when 50% of its lifetime has passed
+			RefreshRate: 0.5,
+			AutoRefresh: true,
+		},
+	}
+
+	// Login to AccelByte backend, using admin username/password
+	// This is the first backend API call, so this is the point where the following parameters are used for the first time (and thus get validated):
+	// - Base URL
+	// - IAM Client ID
+	// - IAM Client Secret
+	// - Admin Username
+	// - Admin Password
+
+	err := oAuth20Service.LoginUser(adminUsername, adminPassword)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to log in to AccelByte backend",
+			"Login using admin username/password failed: "+
+				err.Error(),
+		)
+		return
+	}
+
+	/*
+		// Test SDK calls
+
+		userProfileService := &basic.UserProfileService{
+			Client:          factory.NewBasicClient(&configRepository),
+			TokenRepository: tokenRepository,
+		}
+		input := &user_profile.GetUserProfileInfoParams{
+			Namespace: "alaraprime",
+			UserID:    "9765327f993e4e92b319bfa49f2ec956",
+		}
+
+		// Make a call to getMyProfileInfo endpoint through the wrapper
+		userInfo, err := userProfileService.GetUserProfileInfoShort(input)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Unable to get profile info",
+				"Unable to get profile info:"+err.Error(),
+			)
+			return
+		}
+
+		resp.Diagnostics.AddError(
+			"got profile info!",
+			"got profile info: "+userInfo.PublicID,
+		)
+	*/
+
+	// resp.DataSourceData = client
+	// resp.ResourceData = client
 }
 
 func (p *AccelByteProvider) Resources(ctx context.Context) []func() resource.Resource {
