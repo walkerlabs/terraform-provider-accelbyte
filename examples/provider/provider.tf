e8b78d22edd3d73da109f1b98941e4c7014599fe
Mikael Kalms <mikael@kalms.org>
2025-02-02 21:22:35 +0100

    First attempt at generating docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "accelbyte" {
-  # base_url          = "https://<something>.accelbyte.io"    # or set via ACCELBYTE_BASE_URL
-  # iam_client_id     = "<typically a hex string>"            # or set via ACCELBYTE_IAM_CLIENT_ID
-  # iam_client_secret = "<...>"                               # or set via ACCELBYTE_IAM_CLIENT_SECRET
-  # admin_username    = "<typically an email address>"        # or set via ACCELBYTE_ADMIN_USERNAME
-  # admin_password    = "<...>"                               # or set via ACCELBYTE_ADMIN_PASSWORD
+  base_url          = "https://<something>.accelbyte.io" # or set via ACCELBYTE_BASE_URL
+  iam_client_id     = "<typically a hex string>"         # or set via ACCELBYTE_IAM_CLIENT_ID
+  iam_client_secret = "<...>"                            # or set via ACCELBYTE_IAM_CLIENT_SECRET
+  admin_username    = "<typically an email address>"     # or set via ACCELBYTE_ADMIN_USERNAME
+  admin_password    = "<...>"                            # or set via ACCELBYTE_ADMIN_PASSWORD
 }
